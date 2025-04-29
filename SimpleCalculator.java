ce3b9dc572087d0386cf4ff69abca33f49e663b8
Abhishek-S-261 <abhisheksshettar@gmail.com>
2025-04-29 11:01:43 +0530

    Saved Arithmetic

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -24,7 +24,25 @@ public class SimpleCalculator {
         } else {
             System.out.println("Division: Cannot divide by zero");
         }
+        
+        // Power (num1 raised to num2)
+        double power = Math.pow(num1, num2);
+        System.out.println("Power (num1 ^ num2): " + power);
 
+        // Square root
+        if (num1 >= 0) {
+            double sqrtNum1 = Math.sqrt(num1);
+            System.out.println("Square root of num1: " + sqrtNum1);
+        } else {
+            System.out.println("Square root of num1: Cannot calculate square root of a negative number");
+        }
+
+        if (num2 >= 0) {
+            double sqrtNum2 = Math.sqrt(num2);
+            System.out.println("Square root of num2: " + sqrtNum2);
+        } else {
+            System.out.println("Square root of num2: Cannot calculate square root of a negative number");
+        }
         
         
     }
